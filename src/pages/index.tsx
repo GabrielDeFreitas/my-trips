c1c4bbcb2d497bc8142fd182e4fb620dc1c32dde
gabriel freitas <gabrielfreitas.ilustra@gmail.com>
2023-07-26 14:03:32 -0300

    create linkWrapper component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,14 @@
 import dynamic from 'next/dynamic'
 import Main from 'components/Main'
+import LinkWrapper from 'components/LinkWrapper'
 
 const Map = dynamic(() => import('components/Main/Map'), { ssr: false })
 
 const Index = () => (
   <>
+    <LinkWrapper />
     <Main />
-    <Map
-      places={[
-        {
-          id: '2',
-          name: 'Reykjavik',
-          slug: 'reykjavik',
-          location: {
-            latitude: 45,
-            longitude: -10
-          }
-        }
-      ]}
-    />
+    <Map />
   </>
 )
 

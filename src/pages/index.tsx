ed4cd56f9339a1f7ab34620db508485107f18c19
gabriel freitas <gabrielfreitas.ilustra@gmail.com>
2023-07-26 12:07:14 -0300

    config dynamic map

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,19 @@ const Map = dynamic(() => import('components/Main/Map'), { ssr: false })
 const Index = () => (
   <>
     <Main />
-    <Map />
+    <Map
+      places={[
+        {
+          id: '2',
+          name: 'Reykjavik',
+          slug: 'reykjavik',
+          location: {
+            latitude: 45,
+            longitude: -10
+          }
+        }
+      ]}
+    />
   </>
 )
 

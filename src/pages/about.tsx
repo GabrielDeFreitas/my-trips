9a8f337dc5a6b630d6e5457ad12f2f369f9a03ae
gabriel freitas <gabrielfreitas.ilustra@gmail.com>
2023-07-31 22:51:00 -0300

    getStatic/getServerSide

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,9 +1,21 @@
+import client from 'graphql/client'
+import { GET_PAGES } from 'graphql/queries'
 import AboutTemplate from 'templates/About'
 
-const About = () => (
-  <>
-    <AboutTemplate />
-  </>
-)
+export default function AboutPage() {
+  return <AboutTemplate />
+}
 
-export default About
+export const getStaticProps = async () => {
+  const { pages } = client.request(GET_PAGES)
+
+  console.log(pages)
+
+  return {
+    props: {}
+  }
+}
+
+// getStaticPaths => serve para gerar as urls em build time /about, /trip/florianopolis
+// getStaticProps => serve para buscar dados da pagina (props) - build time - estatico
+// getServerSideProps => serve para buscar dados da pagina ( props) - runtime - toda requisicao (bundle fica no server)
